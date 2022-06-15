381cdda141c1c1ee0958cbfc95d9c880a44659ac
Elliott Yoon <elinate91@gmail.com>
2022-06-15 10:22:58 -0400

    Update view.hxx

--- src/view.hxx
+++ src/view.hxx
@@ -19,17 +19,13 @@ public:
     /// Constructs a view that knows about the given model.
     explicit View(Model const&);
 
-    // TODO: your code here
-    // You will probably want to add arguments here so that the
-    // controller can communicate UI state (such as a mouse or
-    // cursor position):
+
     void draw(ge211::Sprite_set& set);
 
     Dimensions initial_window_dimensions() const;
 
     std::string initial_window_title() const;
 
-    // TODO: Add any public member functions you need.
     View::Position
     board_to_screen(Model::Position logical) const;
 
@@ -38,9 +34,6 @@ public:
 
 private:
     Model const& model_;
-
-    // TODO: Add any additional private members you need, such as helper
-    // functions or sprites.
 
     // sprites
     ge211::Rectangle_sprite const tile_sprite;
